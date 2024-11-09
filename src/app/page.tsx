18dc351b558ae3ad3a3d06aa2c6f3f5ba8fffea6
Guney Unay <guneymarketing17@hotmail.com>
2024-11-09 19:12:18 +0300

    :lipstick: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,72 @@
-import { serverClient } from '../trpc/serverClient'
+'use client'
 
-import Users from '@/components/users'
+import { createAuthClient } from 'better-auth/react'
+import { useState } from 'react'
 
-export const dynamic = 'force-dynamic'
+import { authClient } from '@/lib/auth-client'
 
-export default async function Home() {
-  const users = await serverClient.user.getUsers.query()
+export default function SignUp() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const client = createAuthClient()
+
+  const signUp = async () => {
+    const { data, error } = await authClient.signUp.email(
+      {
+        email,
+        password,
+        name,
+      },
+      {
+        onRequest: (ctx) => {
+          //show loading
+        },
+        onSuccess: (ctx) => {
+          //redirect to the dashboard
+        },
+        onError: (ctx) => {
+          alert(ctx.error.message)
+        },
+      }
+    )
+  }
+
+  const signUpWithGitHub = async () => {
+    const { data, error } = await authClient.signIn.social({
+      provider: 'github',
+    })
+
+    if (error) {
+      alert(error.message)
+    } else {
+      // Redirect to the dashboard or any other page after successful GitHub signup
+    }
+  }
 
   return (
-    <main className="mx-auto mt-5 max-w-3xl">
-      <Users initialData={users} />
-    </main>
+    <div className="flex min-h-screen w-full flex-col gap-4 bg-neutral-200">
+      <input
+        type="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="name"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="email"
+      />
+      <button onClick={signUpWithGitHub}>Sign Up github</button>
+      <button onClick={signUp}>Sign Up</button>
+    </div>
   )
 }
