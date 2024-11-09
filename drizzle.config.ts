c1dc4cb50f8b993670911e2e8b54b07fc181c1e4
Guney Unay <guneymarketing17@hotmail.com>
2024-11-09 17:32:45 +0300

    :card_file_box: update database schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,9 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
+dotenv.config({
+  path: ".env.local",
+});
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
