a5a864fdbba30e21c2f9069b0755555da3e5259a
Guney Unay <guneymarketing17@hotmail.com>
2024-11-12 18:16:04 +0300

    :hammer: added baseUrl func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
