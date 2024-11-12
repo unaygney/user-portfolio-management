7e6ee9e503e963432e2f778527a2bee50290fcaf
Guney Unay <guneymarketing17@hotmail.com>
2024-11-12 15:36:25 +0300

    :sparkles: adde sendEmail feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import { getUserWithProjects } from '@/lib/queries'
-
 export default async function SignUp() {
-  const user = await getUserWithProjects('GEYdxa0YRqNgIbGTeZ4f_')
-  console.log(user)
+  // const user = await getUserWithProjects('GEYdxa0YRqNgIbGTeZ4f_')
+  // console.log(user)
+
+  // const res = await sendEmail('mesafe18@hotmail.com', 'test', 'test')
+  // console.log(res)
 
   return <div>test</div>
 }
