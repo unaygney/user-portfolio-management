f9699ae09cc44f4e48502e1d6481a2aaf909a7c6
Guney Unay <guneymarketing17@hotmail.com>
2024-11-14 20:37:03 +0300

    :bug: fixed

--- src/trpc/serverClient.ts
+++ src/trpc/serverClient.ts
@@ -2,7 +2,7 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 
 import type { AppRouter } from '@/server'
 
-function getBaseUrl() {
+export function getBaseUrl() {
   if (typeof window !== 'undefined')
     // browser should use relative path
     return ''
